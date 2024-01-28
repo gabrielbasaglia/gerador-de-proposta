7f9282d8d29340e2c322d6f8cf809d6f7a97f12d
Gabriel Basaglia <garbaroots@hotmail.com>
2024-01-28 00:19:58 -0300

    formatação dos termos de privacidade

--- src/app/dashboard/privacypolicy/term/page.jsx
+++ src/app/dashboard/privacypolicy/term/page.jsx
@@ -1,62 +1,118 @@
 export default function Term() {
   return (
-    <main className="flex items-center flex-col justify-center min-h-[calc(100vh-10rem)]">
-      <p className="text-center mx-10">
-        1. TermosAo acessar ao site propostamp, concorda em cumprir estes termos
-        de serviço, todas as leis e regulamentos aplicáveise concorda que é
-        responsável pelo cumprimento de todas as leis locais aplicáveis. Se você
-        não concordar com algum desses termos, está proibido de usar ou acessar
-        este site. Os materiais contidos neste site são protegidos pelas leis de
-        direitos autorais e marcas comerciais aplicáveis.2. Uso de LicençaÉ
-        concedida permissão para baixar temporariamente uma cópia dos materiais
-        (informações ou software) no site propostamp , apenas para visualização
-        transitória pessoal e não comercial. Esta é a concessão de uma licença,
-        não uma transferência de título e, sob esta licença, você não pode:
-        modificar ou copiar os materiais; usar os materiais para qualquer
-        finalidade comercial ou para exibição pública (comercial ou não
-        comercial); tentar descompilar ou fazer engenharia reversa de qualquer
-        software contido no site propostamp; remover quaisquer direitos autorais
-        ou outras notações de propriedade dos materiais; ou transferir os
-        materiais para outra pessoa ou 'espelhe' os materiais em qualquer outro
-        servidor.Esta licença será automaticamente rescindida se você violar
-        alguma dessas restrições e poderá ser rescindida por propostamp a
-        qualquer momento. Ao encerrar a visualização desses materiais ou após o
-        término desta licença, você deve apagar todos os materiais baixados em
-        sua posse, seja em formato eletrónico ou impresso.3. Isenção de
-        responsabilidadeOs materiais no site da propostamp são fornecidos 'como
-        estão'. propostamp não oferece garantias, expressas ou implícitas, e,
-        por este meio, isenta e nega todas as outras garantias, incluindo, sem
-        limitação, garantias implícitas ou condições de comercialização,
-        adequação a um fim específico ou não violação de propriedade intelectual
-        ou outra violação de direitos.Além disso, o propostamp não garante ou
-        faz qualquer representação relativa à precisão, aos resultados prováveis
-        ou à confiabilidade do uso dos materiais em seu site ou de outra forma
-        relacionado a esses materiais ou em sites vinculados a este site.4.
-        LimitaçõesEm nenhum caso o propostamp ou seus fornecedores serão
-        responsáveis por quaisquer danos (incluindo, sem limitação, danos por
-        perda de dados ou lucro ou devido a interrupção dos negócios)
-        decorrentes do uso ou da incapacidade de usar os materiais em
-        propostamp, mesmo que propostamp ou um representante autorizado da
-        propostamp tenha sido notificado oralmente ou por escrito da
-        possibilidade de tais danos. Como algumas jurisdições não permitem
-        limitações em garantias implícitas, ou limitações de responsabilidade
-        por danos conseqüentes ou incidentais, essas limitações podem não se
-        aplicar a você.5. Precisão dos materiaisOs materiais exibidos no site da
-        propostamp podem incluir erros técnicos, tipográficos ou fotográficos.
-        propostamp não garante que qualquer material em seu site seja preciso,
-        completo ou atual. propostamp pode fazer alterações nos materiais
-        contidos em seu site a qualquer momento, sem aviso prévio. No entanto,
-        propostamp não se compromete a atualizar os materiais.6. LinksO
-        propostamp não analisou todos os sites vinculados ao seu site e não é
+    <main className="mx-10 p-4">
+      <h2 className="text-gray-700">1. Termos</h2>
+      <p className="text-gray-700">
+        Ao acessar ao site{" "}
+        <a href="https://propostasmp.vercel.app/" className="text-blue-500">
+          Propostasmp
+        </a>
+        , concorda em cumprir estes termos de serviço, todas as leis e
+        regulamentos aplicáveis e concorda que é responsável pelo cumprimento de
+        todas as leis locais aplicáveis. Se você não concordar com algum desses
+        termos, está proibido de usar ou acessar este site. Os materiais
+        contidos neste site são protegidos pelas leis de direitos autorais e
+        marcas comerciais aplicáveis.
+      </p>
+
+      <h2 className="text-gray-700">2. Uso de Licença</h2>
+      <p className="text-gray-700">
+        É concedida permissão para baixar temporariamente uma cópia dos
+        materiais (informações ou software) no site Propostasmp, apenas para
+        visualização transitória pessoal e não comercial. Esta é a concessão de
+        uma licença, não uma transferência de título e, sob esta licença, você
+        não pode:
+      </p>
+      <ol className="list-decimal pl-6 text-gray-700">
+        <li>modificar ou copiar os materiais;</li>
+        <li>
+          usar os materiais para qualquer finalidade comercial ou para exibição
+          pública (comercial ou não comercial);
+        </li>
+        <li>
+          tentar descompilar ou fazer engenharia reversa de qualquer software
+          contido no site Propostasmp;
+        </li>
+        <li>
+          remover quaisquer direitos autorais ou outras notações de propriedade
+          dos materiais; ou
+        </li>
+        <li>
+          transferir os materiais para outra pessoa ou 'espelhe' os materiais em
+          qualquer outro servidor.
+        </li>
+      </ol>
+      <p className="text-gray-700">
+        Esta licença será automaticamente rescindida se você violar alguma
+        dessas restrições e poderá ser rescindida por Propostasmp a qualquer
+        momento. Ao encerrar a visualização desses materiais ou após o término
+        desta licença, você deve apagar todos os materiais baixados em sua
+        posse, seja em formato eletrônico ou impresso.
+      </p>
+
+      <h2 className="text-gray-700">3. Isenção de responsabilidade</h2>
+      <ol className="list-decimal pl-6 text-gray-700">
+        <li>
+          Os materiais no site da Propostasmp são fornecidos 'como estão'.
+          Propostasmp não oferece garantias, expressas ou implícitas, e, por
+          este meio, isenta e nega todas as outras garantias, incluindo, sem
+          limitação, garantias implícitas ou condições de comercialização,
+          adequação a um fim específico ou não violação de propriedade
+          intelectual ou outra violação de direitos.
+        </li>
+        <li>
+          Além disso, o Propostasmp não garante ou faz qualquer representação
+          relativa à precisão, aos resultados prováveis ​​ou à confiabilidade do
+          uso dos materiais em seu site ou de outra forma relacionado a esses
+          materiais ou em sites vinculados a este site.
+        </li>
+      </ol>
+
+      <h2 className="text-gray-700">4. Limitações</h2>
+      <p className="text-gray-700">
+        Em nenhum caso o Propostasmp ou seus fornecedores serão responsáveis por
+        quaisquer danos (incluindo, sem limitação, danos por perda de dados ou
+        lucro ou devido a interrupção dos negócios) decorrentes do uso ou da
+        incapacidade de usar os materiais em Propostasmp, mesmo que Propostasmp
+        ou um representante autorizado da Propostasmp tenha sido notificado
+        oralmente ou por escrito da possibilidade de tais danos. Como algumas
+        jurisdições não permitem limitações em garantias implícitas, ou
+        limitações de responsabilidade por danos consequentes ou incidentais,
+        essas limitações podem não se aplicar a você.
+      </p>
+
+      <h2 className="text-gray-700">5. Precisão dos materiais</h2>
+      <p className="text-gray-700">
+        Os materiais exibidos no site da Propostasmp podem incluir erros
+        técnicos, tipográficos ou fotográficos. Propostasmp não garante que
+        qualquer material em seu site seja preciso, completo ou atual.
+        Propostasmp pode fazer alterações nos materiais contidos em seu site a
+        qualquer momento, sem aviso prévio. No entanto, Propostasmp não se
+        compromete a atualizar os materiais.
+      </p>
+
+      <h2 className="text-gray-700">6. Links</h2>
+      <p className="text-gray-700">
+        O Propostasmp não analisou todos os sites vinculados ao seu site e não é
         responsável pelo conteúdo de nenhum site vinculado. A inclusão de
-        qualquer link não implica endosso por propostamp do site. O uso de
-        qualquer site vinculado é por conta e risco do usuário.ModificaçõesO
-        propostamp pode revisar estes termos de serviço do site a qualquer
+        qualquer link não implica endosso por Propostasmp do site. O uso de
+        qualquer site vinculado é por conta e risco do usuário.
+      </p>
+
+      <p className="mt-4"></p>
+
+      <h3 className="text-gray-700">Modificações</h3>
+      <p className="text-gray-700">
+        O Propostasmp pode revisar estes termos de serviço do site a qualquer
         momento, sem aviso prévio. Ao usar este site, você concorda em ficar
-        vinculado à versão atual desses termos de serviço.Lei aplicávelEstes
-        termos e condições são regidos e interpretados de acordo com as leis do
-        propostamp e você se submete irrevogavelmente à jurisdição exclusiva dos
-        tribunais naquele estado ou localidade.
+        vinculado à versão atual desses termos de serviço.
+      </p>
+
+      <h3 className="text-gray-700">Lei aplicável</h3>
+      <p className="text-gray-700">
+        Estes termos e condições são regidos e interpretados de acordo com as
+        leis do Propostasmp e você se submete irrevogavelmente à jurisdição
+        exclusiva dos tribunais naquele estado ou localidade.
       </p>
     </main>
   );
