83881fef9a048bc346dc36962a28f98fba03d1ad
Gabriel Basaglia <garbaroots@hotmail.com>
2024-01-27 23:40:27 -0300

    pagina de politica de privacidade correção2

--- src/app/dashboard/privacypolicy/page.jsx
+++ src/app/dashboard/privacypolicy/page.jsx
@@ -11,29 +11,29 @@ export default function PrivacyPolicy() {
         informamos por que estamos coletando e como será usado.Apenas retemos as
         informações coletadas pelo tempo necessário para fornecer o serviço
         solicitado. Quando armazenamos dados, protegemos dentro de meios
-        comercialmente aceitáveis ​​para evitar perdas e roubos, bem como
-        acesso, divulgação, cópia, uso ou modificação não autorizados.Não
-        compartilhamos informações de identificação pessoal publicamente ou com
-        terceiros, exceto quando exigido por lei.O nosso site pode ter links
-        para sites externos que não são operados por nós. Esteja ciente de que
-        não temos controle sobre o conteúdo e práticas desses sites e não
-        podemos aceitar responsabilidade por suas respectivas políticas de
-        privacidade.Você é livre para recusar a nossa solicitação de informações
-        pessoais, entendendo que talvez não possamos fornecer alguns dos
-        serviços desejados.O uso continuado de nosso site será considerado como
-        aceitação de nossas práticas em torno de privacidade e informações
-        pessoais. Se você tiver alguma dúvida sobre como lidamos com dados do
-        usuário e informações pessoais, entre em contacto connosco.O serviço
-        Google AdSense que usamos para veicular publicidade usa um cookie
-        DoubleClick para veicular anúncios mais relevantes em toda a Web e
-        limitar o número de vezes que um determinado anúncio é exibido para
-        você.Para mais informações sobre o Google AdSense, consulte as FAQs
-        oficiais sobre privacidade do Google AdSense.Utilizamos anúncios para
-        compensar os custos de funcionamento deste site e fornecer financiamento
-        para futuros desenvolvimentos. Os cookies de publicidade comportamental
-        usados ​​por este site foram projetados para garantir que você forneça
-        os anúncios mais relevantes sempre que possível, rastreando anonimamente
-        seus interesses e apresentando coisas semelhantes que possam ser do seu
+        comercialmente aceitáveis para evitar perdas e roubos, bem como acesso,
+        divulgação, cópia, uso ou modificação não autorizados.Não compartilhamos
+        informações de identificação pessoal publicamente ou com terceiros,
+        exceto quando exigido por lei.O nosso site pode ter links para sites
+        externos que não são operados por nós. Esteja ciente de que não temos
+        controle sobre o conteúdo e práticas desses sites e não podemos aceitar
+        responsabilidade por suas respectivas políticas de privacidade.Você é
+        livre para recusar a nossa solicitação de informações pessoais,
+        entendendo que talvez não possamos fornecer alguns dos serviços
+        desejados.O uso continuado de nosso site será considerado como aceitação
+        de nossas práticas em torno de privacidade e informações pessoais. Se
+        você tiver alguma dúvida sobre como lidamos com dados do usuário e
+        informações pessoais, entre em contacto connosco.O serviço Google
+        AdSense que usamos para veicular publicidade usa um cookie DoubleClick
+        para veicular anúncios mais relevantes em toda a Web e limitar o número
+        de vezes que um determinado anúncio é exibido para você.Para mais
+        informações sobre o Google AdSense, consulte as FAQs oficiais sobre
+        privacidade do Google AdSense.Utilizamos anúncios para compensar os
+        custos de funcionamento deste site e fornecer financiamento para futuros
+        desenvolvimentos. Os cookies de publicidade comportamental usados por
+        este site foram projetados para garantir que você forneça os anúncios
+        mais relevantes sempre que possível, rastreando anonimamente seus
+        interesses e apresentando coisas semelhantes que possam ser do seu
         interesse.Vários parceiros anunciam em nosso nome e os cookies de
         rastreamento de afiliados simplesmente nos permitem ver se nossos
         clientes acessaram o site através de um dos sites de nossos parceiros,
@@ -54,7 +54,7 @@ export default function PrivacyPolicy() {
         mencionado anteriormente, se houver algo que você não tem certeza se
         precisa ou não, geralmente é mais seguro deixar os cookies ativados,
         caso interaja com um dos recursos que você usa em nosso site.Esta
-        política é efetiva a partir de 28 January 2024 02:09
+        política é efetiva a partir de 28 January 2024 02:09
       </p>
     </main>
   );
